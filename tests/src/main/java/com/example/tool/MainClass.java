76b92c94aef25e51fd09b2a3ae669e25d0612538
Dmitry Eremin <mitya1705@gmail.com>
2023-10-09 20:03:51 +0300

    no kafak

---
+++ tests/src/main/java/com/example/tool/MainClass.java
@@ -0,0 +1,18 @@
+package com.example.tool;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+import java.util.concurrent.TimeoutException;
+
+public class MainClass {
+
+
+
+}
